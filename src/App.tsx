5ae84fc226ab2f11c27e5108852383d3963e9a5b
Alejandro Feijóo <afeijoo@encamina.com>
2022-11-27 19:23:28 +0100

    Atomized App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,22 @@
 import { useEffect, useState } from 'react'
-import { getPokemonAsync, getPokemonListAsync } from './api/pokeApi'
+import { getPokemonAsync } from './api/pokeApi'
+import LanguageSelector from './components/LanguageSelector'
 import PokemonList from './components/PokemonList'
-import { areTranslationsCorrect, lang } from './translations/language'
+import { lang } from './translations/language'
 
 function App() {
-
-  const [language, setLanguage] = useState<string>("en")
-
-  const changeLanguage = (lang: string) => {
-    setLanguage(lang)
-  }
-
-  useEffect(() => {
-
-    //Details
-    getPokemonAsync("bulbasaur").then((res) => {
-      console.log(res);
-    }).catch((err) => console.log(err))
-  }, [])
-
-
   return (
     //Componente: selector de idiomas
     //Título de la página
     //Componente: Barra de busqueda por palabras
     //Filtros
-    //spinner
-    //Componente: Lista de pokemon > Componente: Tranjeta de pokemon
     <div>
-      <h1>{lang(language).MessageHello}</h1>
-      {areTranslationsCorrect() && <div>
-        <button onClick={() => changeLanguage("es")}>ES</button>
-        <button onClick={() => changeLanguage("en")}>EN</button>
-      </div>}
+      {/* <h1>{lang(language).MessageHello}</h1> */}
 
+      <LanguageSelector />
       <PokemonList />
 
     </div>
-
   )
 }
 
